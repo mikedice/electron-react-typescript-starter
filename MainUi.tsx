aa37394f9cc819d6a47fd06dc88ae74795a174d2
Mike Dice <mike@Mikes-iMac.local>
2017-06-18 18:00:10 -0700

    getting this working

--- MainUi.tsx
+++ MainUi.tsx
@@ -2,7 +2,13 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import Hello from "./Hello";
 
-ReactDOM.render(
-  <Hello name="Willson" />,
-  document.getElementById("root")
-);
+class UIEntry{
+  static StartUI(){
+    ReactDOM.render(
+      <Hello name="Willson" />,
+      document.getElementById("root")
+    );
+  }
+}
+
+UIEntry.StartUI();
